dc6158c6b7b2ad58d11914f4ce12df0a0096427e
Felicia Becerra <feliciahime@gmail.com>
2020-09-24 15:47:08 -0700

    Confirmed Python script works.

--- build.py
+++ build.py
@@ -4,24 +4,38 @@
 
 print('This is my Python script for re-generasting all three website pages.')
 
-python3 open('./templates/top.html').read()
+print('First create all your variables.')
 
+# First step: create a variable with the top.html:
+open('./templates/top.html').read()
 top_html = open('./templates/top.html').read()
 
-python3 open('./templates/bottom.html').read()
-
+# Second step: create a variable with the bottom.html:
+open('./templates/bottom.html').read()
 bottom_html = open('./templates/top.html').read()
 
-python3 open('./content/index.html').read()
-
+# Third step: create a variable with the index contents:
+open('./content/index.html').read()
 index_contents = open('./content/index.html').read()
 
-python3 open('./content/blog.html').read()
+# Fourth step: create a variable with the about contents:
+open('./content/about.html').read()
+about_contents = open('./content/about.html').read() 
 
-blog_contents = open('./content/blog.html').read()
-
-python3 open('./content/portfolio.html').read()
-
+# Fifth step: create a variable with the portfolio contents:
+open('./content/portfolio.html').read()
 port_contents = open('./content/portfolio.html').read()
+ 
+print('Next build each page.')
 
-python
+# Build the index.html:
+index_html = top_html + index_contents + bottom_html
+open('./docs/index.html', '+w').write(index_html)
+
+# Build the portfolio.html:
+portfolio_html = top_html + port_contents + bottom_html
+open('./docs/portfolio.html', '+w').write(portfolio_html)
+
+# Build the about.html:
+about_html = top_html + about_contents + bottom_html
+open('./docs/about.html', '+w').write(about_html)
