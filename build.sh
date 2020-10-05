1143d0c17864ddff11ec4b7161bae00e26fa26e5
feliciahime <feliciahime@gmail.com>
2020-10-05 14:41:18 -0700

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-# Bash script to generate all three static pages:
-# Felicia Becerra
-# 2020-09-24
-
-cat ./templates/top.html ./content/portfolio.html ./templates/bottom.html > ./docs/portfolio.html
-
-cat ./templates/top.html ./content/about.html ./templates/bottom.html > ./docs/about.html
-
-cat ./templates/top.html ./content/index.html ./templates/bottom.html > ./docs/index.html
